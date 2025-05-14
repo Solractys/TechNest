8fbc89e65cdfa5005a29c608669dcb94bfee13c9
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-05-14 13:59:05 -0300

    restore

--- next.config.js
+++ next.config.js
@@ -2,15 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: "standalone",
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["images.unsplash.com"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
